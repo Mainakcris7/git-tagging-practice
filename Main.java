23b07cefb31d61d53b3e1d062f637b90005c5e71
Mainak Mukherjee <mainakcr72002@gmail.com>
2024-12-18 15:20:42 +0530

    add println for addition and subtraction

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        
+        Calculator calculator = new Calculator();
+        System.out.println("Addition of 10 and 5 " + calculator.add(10, 5));
+        System.out.println("Subtraction of 7 and 4 " + calculator.subtraction(10, 5));
     }
 }
