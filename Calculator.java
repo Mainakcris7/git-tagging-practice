938dd306accfabef6bfd9b81cca0c6d54c78999e
Mainak Mukherjee <mainakcr72002@gmail.com>
2024-12-18 15:41:06 +0530

    double datatype support added

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,15 @@ public class Calculator {
         return a + b;
     }
 
+    public double add(double a, double b) {
+        return a + b;
+    }
+
     public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public double subtraction(double a, double b) {
         return a - b;
     }
 
@@ -11,7 +19,20 @@ public class Calculator {
         return a * b;
     }
 
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
     public int division(int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Exception: can't " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public double division(double a, double b) {
         try {
             return a / b;
         } catch (ArithmeticException e) {
--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println("Addition of 10 and 5 is: " + calculator.add(10, 5));
-        System.out.println("Subtraction of 7 and 4 is: " + calculator.subtraction(10, 5));
-        System.out.println("Multiplication of 2 and 3 is: " + calculator.multiply(2, 3));
-        System.out.println("Division of 8 and 2 is: " + calculator.division(8, 2));
+        System.out.println("Addition of 10.2 and 5 is: " + calculator.add(10.2, 5));
+        System.out.println("Subtraction of 10.2 and 8.3 is: " + calculator.subtraction(10.2, 8.3));
+        System.out.println("Multiplication of 2.34 and 3 is: " + calculator.multiply(2.34, 3));
+        System.out.println("Division of 8.5 and 0.5 is: " + calculator.division(8.5, 0.5));
     }
 }
